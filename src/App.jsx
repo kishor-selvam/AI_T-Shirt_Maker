af27f57794bf5abaccd1d7d765e48fc2b5ce3407
Kishor S <kishorsbca@gmail.com>
2023-10-26 19:10:46 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import Home from "./pages/Home"
 function App() {
 
   return (
+    <>
     <main className="app transition-all eash-in">
       <Home />
       <CanvasModel />
       <Customizer />
     </main>
+    <footer className="flex items-center justify-center">
+    Copyright &copy; 2023.Developed By <a className="pl-1 text-yellow-700" href="https://github.com/kishor-selvam/AI_T-Shirt_Maker/tree/master" target="_black" > Kishor</a>
+    </footer>
+    </>
   )
 }
 
